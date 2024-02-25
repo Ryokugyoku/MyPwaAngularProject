7eb7cee3a96334a4421a170a3ca722b8aa56090c
Ryokugyoku <ryoku@ryokugyoku.tech>
2024-02-25 19:38:39 +0900

    画面修正とPWA化

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-body',
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
+
+
 export class AppComponent {
-  title = 'PwaProject';
+  title = 'Server Manager';
 }
---
+++ src/app/sidebar/app.sidebar.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+@Component({
+    selector: 'app-sidebar',
+    templateUrl: './app.sidebar.html',
+    standalone: true,
+    styleUrls: ['./app.sidebar.scss']
+})
+export class SidebarComponent implements OnInit {
+
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+}
