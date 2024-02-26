fe8c7dfa05e09c5e20489717b839f6543ebd1dae
Ryokugyoku <ryoku@ryokugyoku.tech>
2024-02-26 22:54:49 +0900

    ソースコードの追記

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import {FormsModule} from "@angular/forms";
+import {$localize} from "@angular/localize/init";
 @Component({
   selector: 'app-body',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 
 
-export class AppComponent {
-  title = 'Server Manager';
+export class AppComponent{
+  get selectedLanguage(): string {
+    return this._selectedLanguage;
+  }
+
+  set selectedLanguage(value: string) {
+    this._selectedLanguage = value;
+  }
+  protected _selectedLanguage : string = "";
+  title = $localize`サーバ管理用WEBページβ`
+  changeLanguage() {
+    $localize.locale = this.selectedLanguage;
+    window.location.reload();
+  }
+
 }
+
